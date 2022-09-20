bd28b64d1f7131dca27f60579da41f016a7129ef
dinh0910 <dinhpoor0910@gmail.com>
2022-09-20 14:41:30 +0700

    third commit

--- src/views/example/myComponent.js
+++ src/views/example/myComponent.js
@@ -4,9 +4,20 @@ class MyComponents extends React.Component{
     state = {
         name: 'Vinh'
     }
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render(){
         return(
-            <div>Hello, {this.state.name}</div>
+            <>
+                <input value={this.state.name} typet="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                />
+                <div>Hello, {this.state.name}</div>
+            </>
+            
         )
     }
 }
